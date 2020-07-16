7251205c0dea75f0c48179b11c051a8c50db51df
zhen_zhang <zhen.zhang@idiaoyan.com>
2020-07-16 18:30:49 +0800

    添加spring boot 依赖

--- ehcache/src/main/java/cache/Cache1.java
+++ ehcache/src/main/java/cache/Cache1.java
@@ -1,6 +1,5 @@
 package cache;
 
-import org.ehcache.Cache;
 import org.ehcache.CacheManager;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
